943f3fcf011f770658f36c0a322311568d6997d8
Youngdong <48584373+Youngdong2@users.noreply.github.com>
2024-02-26 21:26:38 +0900

    Title: 함수를 이용한 소수 판별, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240226/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240226/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -1,9 +1,12 @@
 def is_prime(n):
-    for i in range(2, n):
-        if n%i==0:
-            return False
+    if n==1:
+        return False
+    else:
+        for i in range(2, n):
+            if n%i==0:
+                return False
 
-    return True
+        return True
 
 
 def main(a, b):
