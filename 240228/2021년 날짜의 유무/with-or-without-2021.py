df2a1380f07a5827db99f69312e2440f922c830e
Youngdong <48584373+Youngdong2@users.noreply.github.com>
2024-02-28 20:42:10 +0900

    Title: 2021년 날짜의 유무, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240228/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,76 @@
+M, D = map(int, input().split())
+
+if M==1:
+    if 1<=D<=31:
+        print('Yes')
+    else:
+        print('No')
+
+elif M==2:
+    if 1<=D<=28:
+        print('Yes')
+    else:
+        print('No')
+
+elif M==3:
+    if 1<=D<=31:
+        print('Yes')
+    else:
+        print('No')
+
+elif M==4:
+    if 1<=D<=30:
+        print('Yes')
+    else:
+        print('No')
+
+elif M==5:
+    if 1<=D<=31:
+        print('Yes')
+    else:
+        print('No')
+
+elif M==6:
+    if 1<=D<=30:
+        print('Yes')
+    else:
+        print('No')
+
+elif M==7:
+    if 1<=D<=31:
+        print('Yes')
+    else:
+        print('No')
+
+elif M==8:
+    if 1<=D<=31:
+        print('Yes')
+    else:
+        print('No')
+
+elif M==9:
+    if 1<=D<=30:
+        print('Yes')
+    else:
+        print('No')
+
+elif M==10:
+    if 1<=D<=31:
+        print('Yes')
+    else:
+        print('No')
+
+elif M==11:
+    if 1<=D<=30:
+        print('Yes')
+    else:
+        print('No')
+
+elif M==12:
+    if 1<=D<=31:
+        print('Yes')
+    else:
+        print('No')
+
+else:
+    print('No')
