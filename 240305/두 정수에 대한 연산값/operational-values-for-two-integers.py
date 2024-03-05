719935e8776f0a81f7c09a351f790ad2f6223cae
Youngdong <48584373+Youngdong2@users.noreply.github.com>
2024-03-05 20:53:19 +0900

    Title: 두 정수에 대한 연산값, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240305/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 240305/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -1,10 +1,18 @@
-def temp(a, b):
-    M = max(a, b)+25
-    m = min(a, b)*2
+a, b = tuple(map(int, input().split()))
 
-    return M, m
 
-a, b = map(int, input().split())
+# 문제에서 설명한 대로 a와 b를 변경합니다.
+def change_number(a, b):
+    if(a > b):
+        b *= 2
+        a += 25
+    else:
+        a *= 2
+        b += 25
 
-M, m = temp(a, b)
-print(f"{m} {M}")
+    return a, b
+
+
+a, b = change_number(a, b)
+
+print(a, b)
