3600dee8dde26b85e470b2b8ed6817d76bf914eb
Youngdong <48584373+Youngdong2@users.noreply.github.com>
2024-03-13 20:56:48 +0900

    Title: 2개씩 그룹짓기, Time: 167ms, Memory: 27MB, Status: Passed - CodeTree

--- 240313/2개씩 그룹짓기/group-of-pairs.py
+++ 240313/2개씩 그룹짓기/group-of-pairs.py
@@ -1,13 +1,7 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-arr = sorted(arr)
-a1 = []
-a2 = []
-for i in range(2*n):
-    if i%2==0:
-        a1.append(arr[i])
-    else:
-        a2.append(arr[i])
+arr1 = sorted(arr)
+arr2 = sorted(arr, reverse=True)
 
-print(max(sum(a1), sum(a2)))
+print(max([a+b for a, b in zip(arr1, arr2)]))
