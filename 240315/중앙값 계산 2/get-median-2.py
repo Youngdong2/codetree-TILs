beedaebc1f130f1eb0c41d012786b7949bbe99fa
Youngdong <48584373+Youngdong2@users.noreply.github.com>
2024-03-15 20:19:22 +0900

    Title: 중앙값 계산 2, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240315/중앙값 계산 2/get-median-2.py
+++ 240315/중앙값 계산 2/get-median-2.py
@@ -4,6 +4,6 @@ arr = list(map(int, input().split()))
 answer = []
 for i in range(n):
     if i%2==0:
-        answer.append(arr[:i+1][i//2])
+        answer.append(sorted(arr[:i+1])[i//2])
 
 print(*answer)
