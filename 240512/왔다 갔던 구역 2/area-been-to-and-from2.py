ce22db08a95786c756ea2dd84362085aaa2b484c
Youngdong <48584373+Youngdong2@users.noreply.github.com>
2024-05-12 13:52:19 +0900

    Title: 왔다 갔던 구역 2, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240512/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240512/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,24 +1,28 @@
 n = int(input())
 
+# 좌표 범위를 설정합니다.
 arr = [0]*2000
 offset = 1000
+state = offset
 
-for i in range(n):  
-    x, d = input().split()
-    state = offset
-    if d=='R':
-        for j in range(state, state+int(x)-1):
+for i in range(n):
+    x, direction = input().split()
+    x = int(x)
+    
+    # 오른쪽으로 이동하는 경우
+    if direction == 'R':
+        for j in range(state, state + x):
             arr[j] += 1
+        state += x  # 상태 업데이트
 
-        state = state + int(x)-1
-
+    # 왼쪽으로 이동하는 경우
     else:
-        for j in range(state-int(x), state-1):
+        for j in range(state - x, state):
             arr[j] += 1
-
-        state = state - int(x)-1
+        state -= x  # 상태 업데이트
 
 count = 0
+# 2번 이상 방문한 위치를 세는 로직
 for a in arr:
     if a >= 2:
         count += 1
