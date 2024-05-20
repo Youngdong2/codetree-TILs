77b6ff60ab6df56c674a4e7f22c863aa37e1a2ba
Youngdong <48584373+Youngdong2@users.noreply.github.com>
2024-05-20 21:12:15 +0900

    Title: 벌금은 누구에게, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 240520/벌금은 누구에게/who-will-pay.py
+++ 240520/벌금은 누구에게/who-will-pay.py
@@ -1,16 +1,14 @@
 n, m, k = map(int, input().split())
-student_num = [0]*n
-
-
-for _ in range(m):
-    num = int(input())
-    student_num[num-1] += 1
+student_penalties = [0]*n
 
 
 answer = -1
-for i in range(n):
-    if student_num[i] >= k:
-        answer = i+1
+
+for _ in range(m):
+    student_number = int(input())-1
+    student_penalties[student_number]+=1
+    if student_penalties[student_number] == k:
+        answer = student_number + 1
         break
 
 print(answer)
