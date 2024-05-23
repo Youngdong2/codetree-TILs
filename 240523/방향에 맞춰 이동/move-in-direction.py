2fcc7693999bc7c03879a923df4aa668e0c35bfd
Youngdong <48584373+Youngdong2@users.noreply.github.com>
2024-05-23 20:58:30 +0900

    Title: 방향에 맞춰 이동, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240523/방향에 맞춰 이동/move-in-direction.py
@@ -0,0 +1,13 @@
+n = int(input())
+x, y = 0, 0
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
+direction_map = {"N":3, "E":0, "S":1, "W": 2}
+
+for _ in range(4):
+    direction, r = input().split()
+    r = int(r)
+    x = x + dx[direction_map[direction]]*r
+    y = y + dy[direction_map[direction]]*r
+
+print(x, y)
