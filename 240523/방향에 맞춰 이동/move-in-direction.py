07fe89e5054ac75ee9dfac45b45e467e4d4de607
Youngdong <48584373+Youngdong2@users.noreply.github.com>
2024-05-23 20:59:58 +0900

    Title: 방향에 맞춰 이동, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240523/방향에 맞춰 이동/move-in-direction.py
+++ 240523/방향에 맞춰 이동/move-in-direction.py
@@ -4,7 +4,7 @@ dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
 direction_map = {"N":3, "E":0, "S":1, "W": 2}
 
-for _ in range(4):
+for _ in range(n):
     direction, r = input().split()
     r = int(r)
     x = x + dx[direction_map[direction]]*r
