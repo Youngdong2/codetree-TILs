aaea221dea00ddf383f7d0ad51b953cc4218c1ec
Youngdong <48584373+Youngdong2@users.noreply.github.com>
2024-10-19 14:09:20 +0900

    Title: 거품 정렬 구현, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241019/거품 정렬 구현/implement-bubble-sort.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(n):
+    for j in range(n-1):
+        if arr[j] > arr[j+1]:
+            temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
+
+
+print(*arr)
